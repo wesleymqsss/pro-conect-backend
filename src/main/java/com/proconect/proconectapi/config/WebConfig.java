9b706d1e2712b8604a741d0bfd759d7a61d37b12
Wesley Oliveira <Wesleystarwars123@gmail.com>
2025-05-28 13:22:49 -0300

    feat: adiçao de config para api

---
+++ src/main/java/com/proconect/proconectapi/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.proconect.proconectapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // <-- Corrigido de "/*" para "/**"
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*") // <-- Corrigido de "" para "*" (aceitar qualquer header)
+                .allowCredentials(false);
+    }
+}
