0a431a659db3ad16a891d2c006a95d210e09e01f
Wesley Oliveira <Wesleystarwars123@gmail.com>
2025-06-05 11:30:04 -0300

    feat: Ajuste na rota de turmas

--- src/main/java/com/proconect/proconectapi/controller/AlunoController.java
+++ src/main/java/com/proconect/proconectapi/controller/AlunoController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/Alunos")
+@RequestMapping("/api/aluno")
 public class AlunoController {
 
     private final AlunoRepository alunoRepo;
